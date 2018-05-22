f1c394d0e6bc14f19fc866ce5b161e78cc515e95
dip4k <dip4k.chauhan@outlook.com>
2018-05-22 12:05:20 +0530

    Divide Main Component into sub Component - use of [@Input,@Output]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,29 @@ import { Todo } from './todo';
 })
 export class AppComponent {
   title = 'app';
-  newTodo: Todo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // newTodo: Todo = new Todo();
 
   constructor(private todoDataService: TodoDataService) {}
 
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
+
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
 
-  toggleTodoComplete(todo) {
+  // rename from toggleTodoComplete
+  onToggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
+  // rename from removeTodo
+  onRemoveTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { TodoDataService } from './services/todo-data.service';
 
 import { AppComponent } from './app.component';
 import { TodoListHeaderComponent } from './todoListComponents/todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todoListComponents/todo-list/todo-list.component';
+import { TodoListItemComponent } from './todoListComponents/todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todoListComponents/todo-list-footer/todo-list-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListHeaderComponent],
+  declarations: [AppComponent, TodoListHeaderComponent, TodoListComponent, TodoListItemComponent, TodoListFooterComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [TodoDataService],
   bootstrap: [AppComponent]
